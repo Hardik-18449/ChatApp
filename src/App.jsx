c97b36f301a6cc5f6c58c9116475df28f73c417b
Hardik <hardikgurjar174@gmail.com>
2025-08-08 19:32:12 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import ProfilePage from './pages/ProfilePage'
+import bgImage from './assets/bgImage.svg'
+
+const App = () => {
+  return (
+    <div style={{ backgroundImage: `url(${bgImage})` }} className="min-h-screen bg-no-repeat bg-center bg-cover">
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
